13d0027329c681004f4bfc81c2b56bb9f766b890
BryanOlivares <bpatricio155@gmail.com>
2021-01-15 13:16:41 -0500

    Conexión con Firebase

--- examen1_BO/src/environments/environment.ts
+++ examen1_BO/src/environments/environment.ts
@@ -6,6 +6,18 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCfbuQGDFR1urqiOBDU0Lwi3D0VimomX7c",
+  authDomain: "examen1te-5dca4.firebaseapp.com",
+  projectId: "examen1te-5dca4",
+  storageBucket: "examen1te-5dca4.appspot.com",
+  messagingSenderId: "545030070824",
+  appId: "1:545030070824:web:958c5f6719a5a747494b1d",
+  measurementId: "G-4E9FQ5G1DW"
+};
+
+
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
